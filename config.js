eaeecb742149762102206f4b212a629307ad6897
Sudry <108238011+SudryDevv@users.noreply.github.com>
2024-02-02 17:19:40 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    token: "YOUR_BOT_TOKEN", // https://discord.com/developers/applications
+    prefix: "YOUR_PREFIX",
+}
---
+++ install.bat
@@ -0,0 +1,4 @@
+echo off
+npm i axios@1.6.7
+npm i crypto@1.0.1
+npm i discord.js@13.15.1
